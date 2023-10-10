493058a504eae83493fc95898511f8175c596cc4
vinaykangralkar <vinaykangralkar9@gmail.com>
2023-10-10 15:41:10 +1100

    Update server.js

--- express-server/server.js
+++ express-server/server.js
@@ -41,7 +41,7 @@ app.put('/entries/:id', (req, res) => {
 app.delete('/delete/:id', (req, res) => {
   const id = parseInt(req.params.id);
 
-  entries = entries.filter((entry) => entry.id !== id);
+  entries = entries.filter((entry) => entry.id !== String(id));
 
   console.log('Entry deleted:', entries)
   res.json({ message: 'Entry deleted successfully' });
