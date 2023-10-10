d60cf89f05e467b0ea45793af6cb8e5ba81c80bb
vinaykangralkar <vinaykangralkar9@gmail.com>
2023-10-10 15:29:37 +1100

    Revert "Update server.js"
    This reverts commit 0684a2dc4e119ea182f2ca7bffe6a11ca7cef4c1.

--- express-server/server.js
+++ express-server/server.js
@@ -11,23 +11,7 @@ app.use(cors());
 app.use(bodyParser.json());
 
 // Dummy data to simulate a database
-let entries = [
-    {
-      "id": '1',
-      "title": "Title 1",
-      "description": "Description 1"
-    },
-    {
-      "id": '2',
-      "title": "Title 2",
-      "description": "Description 2"
-    },
-    {
-      "id": '3',
-      "title": "Title 3",
-      "description": "Description 3"
-    }
-  ];
+let entries = [];
 
 // Endpoint for creating an entry
 app.post('/create', (req, res) => {
